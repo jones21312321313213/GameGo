cbec5a5730094c7b6b82025db66939e61ce6fc2f
jones21312321313213 <skinnyjones212121@gmail.com>
2025-02-19 17:44:40 +0800

    Add files via upload

---
+++ GameGo3/app/src/main/java/cit/edu/gamego/LoginActivity.kt
@@ -0,0 +1,27 @@
+package cit.edu.gamego
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register_f3);
+
+        val button_register = findViewById<Button>(R.id.login_Id);
+        button_register.setOnClickListener {
+            Log.e("CSIT 284", "absolute cinema");
+            Toast.makeText(this,"absolute dogshit",Toast.LENGTH_LONG).show();
+            val intent = Intent(this,RegisterActivity::class.java);
+            startActivity(intent);
+        }
+    }
+}
