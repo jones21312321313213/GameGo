13987cdccf0a8f7b72be9a04cc70026a15b0aaf9
jones21312321313213 <skinnyjones212121@gmail.com>
2025-05-04 08:57:25 +0800

    adsasd

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -143,6 +143,7 @@ dependencies {
     implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.appcompat:appcompat:1.3.1")
 
+
     //imgbb
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
